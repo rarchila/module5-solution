c6f853eb3e4bf63678e9389741f6dd70e8d20ebb
rarchila <archila.ricardo@gmail.com>
2020-08-01 10:13:34 -0500

    agregado console.log

--- js/script.js
+++ js/script.js
@@ -118,6 +118,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
       var homeHtmlToInsertIntoMainPage = dc.loadMenuItems(chosenCategoryShortName);
+      console.log(dc.loadMenuItems(chosenCategoryShortName));
       console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
