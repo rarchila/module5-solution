13548f531ce07977e4cc61d43b2d75c7debdc281
rarchila <archila.ricardo@gmail.com>
2020-08-01 10:21:09 -0500

    console.log

--- js/script.js
+++ js/script.js
@@ -152,6 +152,7 @@ dc.loadMenuCategories = function () {
 // Load the menu items view
 // 'categoryShort' is a short_name for a category
 dc.loadMenuItems = function (categoryShort) {
+  console.log("categoria corta",categoryShort);
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     menuItemsUrl + categoryShort,
